6975a5b7ecd577f33d0d9713587d717f03fcb68b
ulianaboikova <ulyanaboykova49@gmail.com>
2025-02-05 01:57:06 -0800

    Initial commit

---
+++ src/test/java/com/sitestart/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitestart.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
