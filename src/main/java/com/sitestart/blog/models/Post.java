7d9b7c7007f23832e27a3574385f8598d5c5d5d7
UlianaBoikova <ulyanaboykova49@gmail.com>
2025-10-27 13:25:39 -0700

    Update Post.java

--- src/main/java/com/sitestart/blog/models/Post.java
+++ src/main/java/com/sitestart/blog/models/Post.java
@@ -1,5 +1,6 @@
 package com.sitestart.blog.models;
 
+import jakarta.persistence.Lob;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
